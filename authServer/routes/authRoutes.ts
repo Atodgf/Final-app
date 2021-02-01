44af1125a25b0e4e0c8e6005a9ec73eabf232db5
fgdota2 <68871109+fgdota2@users.noreply.github.com>
2021-02-01 13:49:52 +0300

    hashing password

--- authServer/routes/authRoutes.ts
+++ authServer/routes/authRoutes.ts
@@ -21,4 +21,21 @@ router.post('/signup', async (req, res) =>{
     }
 })
 
+router.post('/signin',async (req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.status(422).send({error :"must provide email or password"})
+    }
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(422).send({error :"must provide email or password"})
+    }
+    try{   
+      const token = jwt.sign({userId:user._id},jwtkey)
+      res.send({token})
+    }catch(err){
+        return res.status(422).send({error :"must provide email or password"})
+    }
+})
+
 export default router
